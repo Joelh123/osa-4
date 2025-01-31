5b5a35996ba2ea72a4af1d07ac461de812c37e10
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-01-31 12:07:39 +0200

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((accumulator, currentValue) => accumulator + currentValue.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let favorite = { likes: 0 }
+    blogs.forEach(blog => {
+        blog.likes > favorite.likes ? favorite = blog : favorite
+    });
+    return favorite
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
