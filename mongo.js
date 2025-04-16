929adb3b35d1b9e2af9e3834b28da62061ea453b
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-04-16 09:35:32 +0300

    4.8

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://joelhheikkinen:${password}@cluster0.ryjqz.mongodb.net/testBlogsApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url).then(() => {
+  const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+
+  const Blog = mongoose.model('Blog', blogSchema)
+
+  /*
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  Blog.find({}).then(result => {
+    result.forEach(blog => {
+      console.log(blog)
+    })
+    mongoose.connection.close()
+  })
+})
---
+++ tests/blog_test.test.js
@@ -0,0 +1,70 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+  
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+  })
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    assert(titles.includes('Kalat'))
+})
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: "Uusi",
+        author: "John Doe",
+        url: "google.com",
+        likes: 2
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    assert(titles.includes('Uusi'))
+})
+
+test('a blog can be deleted', async () => {
+    await api
+        .delete('/api/blogs/67f8ac479cb3cd6e3bf25233')
+        .expect(204)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
