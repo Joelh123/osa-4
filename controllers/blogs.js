6f1307cb9a3399f1f8f1e57a299b0c5c847a6e10
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-05-06 08:58:13 +0300

    4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -42,6 +42,18 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+
+    if (user._id.toString() !== blog.user._id.toString()) {
+        return response.status(401).json({ error: 'unauthorized access' })
+    }
+
     await Blog.findByIdAndDelete(request.params.id)
     response.status(204).end()
 })
